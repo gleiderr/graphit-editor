709150db69d4ece46afcdb25d5c387a43212a7ce
Gleider Costa - Getin <gleider.costa@alterosa.CONSERVO>
2019-05-31 13:00:15 -0300

    Escrevendo um cenário que englobe os demais +Testando posição de campos

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -7,6 +7,23 @@ const firebase = require('firebase');
 let browser;
 const test_ref = '__graphit-test__';
 
+const type = async (string, page) => {
+  const focusedField = await page.$(':focus');
+  await focusedField.type(string, {delay: 50});
+}
+
+const keypress = async (key, page) => {
+  await page.keyboard.press(key);
+}
+
+const delay = async (s, page) => {
+  await page.waitFor(1000 * s);
+}
+
+const contents = page => {
+  return page.$$eval('.Graphit-Node', nodes => nodes.map(n => n.innerText));
+}
+
 BeforeAll({timeout: 60 * 1000}, async function () {
   //Acesso à página
   browser = await puppeteer.launch({
@@ -31,8 +48,12 @@ AfterAll(async function() {
   console.log('Fim!');
 });
 
-Given('que não há informação gravada na base de dados', function () {
-  firebase.database().ref(test_ref).remove();
+Given('que não há informação gravada na base de dados', async function () {
+  await firebase.database().ref(test_ref).remove();
+});
+
+Given('a seguinte lista de livros', async function (dataTable) {
+  this.tabelaLivros = dataTable.hashes();
 });
 
 When('acessar página {string}', {timeout: 1000 * 30}, async function (url) {
@@ -41,7 +62,7 @@ When('acessar página {string}', {timeout: 1000 * 30}, async function (url) {
 });
 
 When('aguardar {int} segundo(s)', {timeout: 1000 * 30}, async function (n) {
-  await this.page.waitFor(1000 * n);
+  await delay(n, this.page);
 });
 
 When('clicar sobre o primeiro campo', async function () {
@@ -50,19 +71,57 @@ When('clicar sobre o primeiro campo', async function () {
 });
 
 When('digitar {string}', async function (string) {
-  const focusedField = await this.page.$(':focus');
-  await focusedField.type(string, {delay: 30});
+  await type(string, this.page);
 });
 
 When('fechar a página', async function () {
   await this.page.close();
 });
 
+const assertSome = (innerTexts, test) => {
+  assert.ok(innerTexts.some(innerText => innerText === test), 
+            `Teste: ${test}, Nodos existentes: ${innerTexts}`);
+}
+
 Then('deve existir um campo com o texto {string}', async function (string) {
-  const innerTexts = await this.page.$$eval('.Graphit-Node', ns => ns.map(n => n.innerText));
-  assert.ok(innerTexts.some(innerText => innerText === string), `Nodos existentes: ${innerTexts}`);
+  const innerTexts = await contents(this.page);
+  assertSome(innerTexts, string);
 });
 
 When('teclar "Enter"', async function() {
-  await this.page.keyboard.press('Enter');
+  await keypress('Enter', this.page);
+});
+
+When('digitar os livros teclando {string} com delay de {int} segundo(s):', {timeout: 1000 * 30}, 
+  async function (string, int) {
+    for (const row of this.tabelaLivros) {
+      await keypress('Enter', this.page);
+      await delay(int, this.page);
+      await type(row['livro'], this.page);
+      await delay(int, this.page);
+    }
+  }
+);
+
+Then('deve existir um campo para cada livro da lista', async function () {
+  const innerTexts = await contents(this.page);
+  for (const row of this.tabelaLivros) {
+    assertSome(innerTexts, row['livro']);
+  }
+});
+
+Then('cada livro deve estar com {int} identação', async function (int) {
+
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('o primeiro livro deve estar abaixo de {string}', async function (string) {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
+});
+
+Then('cada livro deve estar abaixo de seu antecessor', async function () {
+  // Write code here that turns the phrase above into concrete actions
+  return 'pending';
 });
--- features/test.feature
+++ features/test.feature
@@ -6,37 +6,59 @@ Funcionalidade: Redação de nodos bíblicos e seus comentários
         #Dado que é aceito um delay de 1 segundo após "acesso a página"
         #E que é aceito um delay de 1 segundo após "terminar de digitar" 
 
-    Esquema do Cenário: Inclusão de texto "<Versão da Bíblia>"
+    #Esquema do Cenário: Inclusão de texto "<Versão da Bíblia>"
+    #    Dado que não há informação gravada na base de dados
+    #    Quando acessar página 'localhost:3000'
+    #    E aguardar 1 segundo
+    #    E clicar sobre o primeiro campo
+    #    E digitar "<Versão da Bíblia>"
+    #    E aguardar 1 segundo
+    #    E fechar a página
+    #    E acessar página 'localhost:3000'
+    #    E aguardar 2 segundos
+    #    Então deve existir um campo com o texto "<Versão da Bíblia>"
+
+    #    Exemplos:
+    #    | Versão da Bíblia |
+    #    | Bíblia Sagrada   |
+    #    | Bíblia King James|
+
+    #Cenário: Inclusão de texto "Evangelho de João" em "Bíblia Sagrada"
+    #    Dado que não há informação gravada na base de dados
+    #    Quando acessar página 'localhost:3000'
+    #    E aguardar 1 segundo
+    #    E clicar sobre o primeiro campo
+    #    E digitar "Bíblia Sagrada"
+    #    E aguardar 2 segundos
+    #    E teclar "Enter"
+    #    E aguardar 1 segundo
+    #    E digitar "Evangelho de João"
+    #    E aguardar 2 segundos
+    #    Então deve existir um campo com o texto "Bíblia Sagrada"
+    #    E deve existir um campo com o texto "Evangelho de João"
+
+    Cenário: Inclusão de título de livros em "Bíblia Sagrada"
         Dado que não há informação gravada na base de dados
+        Dado a seguinte lista de livros
+            |livro              |
+            |Evangelho de João  |
+            |Evangelho de Mateus|
+            |Atos dos Apóstolos |
         Quando acessar página 'localhost:3000'
-        E aguardar 1 segundo
-        E clicar sobre o primeiro campo
-        E aguardar 1 segundo
-        E digitar "<Versão da Bíblia>"
-        E aguardar 1 segundo
-        E fechar a página
-        E acessar página 'localhost:3000'
         E aguardar 2 segundos
-        Então deve existir um campo com o texto "<Versão da Bíblia>"
-
-        Exemplos:
-        | Versão da Bíblia |
-        | Bíblia Sagrada   |
-        | Bíblia King James|
-
-    Cenário: Inclusão de texto "Evangelho de João" em "Bíblia Sagrada"
-        Dado que não há informação gravada na base de dados
-        Quando acessar página 'localhost:3000'
-        E aguardar 1 segundo
         E clicar sobre o primeiro campo
         E digitar "Bíblia Sagrada"
         E aguardar 2 segundos
-        E teclar "Enter"
-        E aguardar 1 segundo
-        E digitar "Evangelho de João"
+        E digitar os livros teclando "Enter" com delay de 1 segundo:
+        E aguardar 2 segundos
+        E fechar a página
+        E acessar página 'localhost:3000'
         E aguardar 2 segundos
         Então deve existir um campo com o texto "Bíblia Sagrada"
-        E deve existir um campo com o texto "Evangelho de João"
+        E deve existir um campo para cada livro da lista
+        E o primeiro livro deve estar abaixo de "Bíblia Sagrada"
+        E cada livro deve estar abaixo de seu antecessor
+        E cada livro deve estar com 1 identação
 
     #Cenário: Exibição de nodo liberado para edição (verde, amarelo, vermelho)
     #Cenário: 
