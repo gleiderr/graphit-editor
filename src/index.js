03617367ee442016c5f50efc2ac651ab73cb8e30
Gleider Ribeiro Costa <gleiderr@gmail.com>
2019-07-03 17:06:42 -0300

    Ajustes para que conteúdo seja identado por \t

--- src/index.js
+++ src/index.js
@@ -256,14 +256,20 @@ class Node extends React.Component {
         }
 
         let tabs = '\t'.repeat(this.props.deep);
+        const tabSize = '2rem';
+        const widthExpression = `calc(${tabSize} * ${this.props.deep})`;
 
         //https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
+        //https://www.w3schools.com/cssref/css3_pr_tab-size.asp
+        //https://developer.mozilla.org/en-US/docs/Web/CSS/tab-size
+        //https://developer.mozilla.org/en-US/docs/Web/CSS/calc
         return (
-            <div>
-                <span style={{'white-space': 'pre-wrap'}}>{tabs}</span>
+            <>
+            <div className="Row">
+                <span className="Indent"style={{tabSize}}>{tabs}</span>
                 <span className="Graphit-Node" 
                      contentEditable suppressContentEditableWarning draggable
-                     //style={style}
+                     style={{width: `calc(calc(100% - 2px) - ${widthExpression})`}}
                      onInput={evt => this.context.inputHandle(this.props.id, evt.target.innerText)}
                      onKeyDown={evt => this.keyDownHandle(evt)}
                      onDragStart={evt => this.dragStartHandle(evt, this.props.id)}
@@ -275,8 +281,9 @@ class Node extends React.Component {
                      ref={this.myInput} >
                         {this.state.data}
                 </span>
-                {nodes}
-            </div>);
+            </div>
+            {nodes}
+            </>);
     }
 }
 
